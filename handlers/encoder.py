3b09d2db194abc1d4369d5c365135eb676185eda
stanisluv <122376860+fluffystuks@users.noreply.github.com>
2025-10-26 12:42:59 +0300

    caeasar encoder in ASCII

--- handlers/encoder.py
+++ handlers/encoder.py
@@ -28,4 +28,21 @@ def atbash_encrypt(text):
         t = abc.index(ch)
         k = abc.index(key[i % len(key)].upper())
         res += abc[(t - k if decrypt else t + k) % len(abc)]
+
     return res
+
+def shift_encrypt_ASCII(text, shift):
+    result = ""
+    for char in text:
+        code = ord(char)
+        if 32 <= code <= 126:
+            if code + shift > 126:
+                new_code = 32 + ((code + shift) - 127)
+            elif code + shift < 32:
+                new_code = 127 - (32 - (code + shift))
+            else:
+                new_code = code + shift
+            result += chr(new_code)
+        else:
+            result += char
+    return result
