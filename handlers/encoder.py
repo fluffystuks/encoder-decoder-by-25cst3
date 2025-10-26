61248241b3e59b122f958e40abdae8541030ae2e
Egor-or-or <petrovegor514@gmail.com>
2025-10-26 11:27:31 +0300

    1.1v vigenere decode

--- handlers/encoder.py
+++ handlers/encoder.py
@@ -3,9 +3,10 @@ def vigenere_decoder(text, key, decrypt=True):
     res = ""
     for i, ch in enumerate(text.upper()):
         if ch not in abc:
-            res = ch
+            res += ch
             continue
         t = abc.index(ch)
         k = abc.index(key[i % len(key)].upper())
         res += abc[(t - k if decrypt else t + k) % len(abc)]
     return res
+
