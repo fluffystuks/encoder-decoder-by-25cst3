45ef6e19a9c424846efe141784ff363ea50ee998
stanisluv <122376860+fluffystuks@users.noreply.github.com>
2025-10-26 12:43:20 +0300

    caesar decoder in ASCII

--- handlers/decoder.py
+++ handlers/decoder.py
@@ -9,3 +9,19 @@ def vigenere_decoder(text, key, decrypt=True):
         k = abc.index(key[i % len(key)].upper())
         res += abc[(t - k if decrypt else t + k) % len(abc)]
     return res
+
+def shift_encrypt_ASCII(text, shift):
+    result = ""
+    for char in text:
+        code = ord(char)
+        if 32 <= code <= 126:
+            if code + shift > 126:
+                new_code = 32 + ((code + shift) - 127)
+            elif code + shift < 32:
+                new_code = 127 - (32 - (code + shift))
+            else:
+                new_code = code + shift
+            result += chr(new_code)
+        else:
+            result += char
+    return result
